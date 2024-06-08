ca1f939de4bdb7e9b5b9fb49961d2429932f52bf
Javi-23 <javieroliasmorales@gmail.com>
2024-06-08 14:55:56 +0200

    08-11

--- TfgSn.App/src/environments/environment.development.ts
+++ TfgSn.App/src/environments/environment.development.ts
@@ -1,4 +1,4 @@
 export const environment = {
     //apiUrl : "https://localhost:7047/api"
-    apiUrl : "http://4.180.7.70/api"
+    apiUrl : "http://4.180.7.70:8080/api"
 };
--- TfgSn.App/src/environments/environment.ts
+++ TfgSn.App/src/environments/environment.ts
@@ -1,3 +1,3 @@
 export const environment = {
-    apiUrl : "http://4.180.7.70/api"
+    apiUrl : "http://4.180.7.70:8080/api"
 };
