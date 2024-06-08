a629d8aa9de87ab5e658f78e0a4e603a4ecb5284
Javi-23 <javieroliasmorales@gmail.com>
2024-06-08 19:55:46 +0200

    09-08

--- ApiTFG/obj/Release/net8.0/ApiTFG.AssemblyInfo.cs
+++ ApiTFG/obj/Release/net8.0/ApiTFG.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ApiTFG")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fcddc853eec9e8a1a47f00e7c145efd213f60775")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+27ec717bb24f66e07b2f6b020875982e15bf618f")]
 [assembly: System.Reflection.AssemblyProductAttribute("ApiTFG")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ApiTFG")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
